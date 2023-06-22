1cde73df6b0012ad97b1ac72a4747f4129c6a3a7
sedi <salehisediqe86@gmail.com>
2023-06-22 09:42:11 -0700

    Created Die component & Styled it

--- src/App.js
+++ src/App.js
@@ -1,10 +1,22 @@
-
+import React from "react";
+import Die from "./Die";
 import './App.css';
 
 export default function App() {
   return (
     <main className="App">
-      <h1></h1>
+      <div className="dice-container">
+        <Die value="1"/>
+        <Die value="2"/>
+        <Die value="3"/>
+        <Die value="4"/>
+        <Die value="5"/>
+        <Die value="6"/>
+        <Die value="7"/>
+        <Die value="8"/>
+        <Die value="9"/>
+        <Die value="10"/>
+      </div>
     </main>
   );
 }
---
+++ src/Die.js
@@ -0,0 +1,11 @@
+import React from "react";
+import "./App.css";
+
+export default function Die (props){
+    return (
+        <div className="die-face">
+            <h2 className="die-num">{props.value}</h2>
+        </div>
+
+    );
+}
