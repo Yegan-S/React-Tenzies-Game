016c3cbc09247e3601e105fbfff3d23d3ed8b885
sedi <salehisediqe86@gmail.com>
2023-06-23 02:30:32 -0700

    Mapped array to Die component

--- src/App.js
+++ src/App.js
@@ -4,27 +4,23 @@ import './App.css';
 
 export default function App() {
 
+  const [dice, setDice] = React.useState(allNewDice())
+
   function allNewDice (){
     const newDice = []
     for (let i = 0; i < 10; i++) {
-      newDice.push(Math.ceil(Math.random() *6))
+      newDice.push(Math.ceil(Math.random() * 6))
     }
     return newDice
   }
-  console.log(allNewDice())
+
+  const diceElements = dice.map( die => <Die value={die} /> )
+
   return (
     <main className="App">
       <div className="dice-container">
-        <Die value="1"/>
-        <Die value="2"/>
-        <Die value="3"/>
-        <Die value="4"/>
-        <Die value="5"/>
-        <Die value="6"/>
-        <Die value="7"/>
-        <Die value="8"/>
-        <Die value="9"/>
-        <Die value="10"/>
+        {diceElements}
+
       </div>
     </main>
   );
